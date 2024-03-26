a3da651e5ac7eba46c9075cc489b3dadf665ae40
Zane Neurothrone <54468049+neurothrone@users.noreply.github.com>
2024-03-26 21:11:08 +0100

    refactor to load and save state on navigation

--- lib/src/navigation/data/navigation_controller.dart
+++ lib/src/navigation/data/navigation_controller.dart
@@ -1,20 +1,45 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../services/preferences/preferences.dart';
 import '../domain/domain.dart';
 import 'navigation_state.dart';
 
 class NavigationController extends StateNotifier<NavigationState> {
-  NavigationController() : super(NavigationState.initial());
+  NavigationController({
+    required IPreferencesService preferencesService,
+  })  : _preferencesService = preferencesService,
+        super(NavigationState.initial());
+
+  final IPreferencesService _preferencesService;
+
+  Future<void> loadSavedState() async {
+    final screen = await _preferencesService.getScreen();
+    final gasTab = await _preferencesService.getGasTab();
+    state = state.copyWith(
+      selectedScreen: screen,
+      selectedGasTab: gasTab,
+    );
+  }
+
+  Future<void> _saveScreen() async {
+    await _preferencesService.setScreen(state.selectedScreen);
+  }
+
+  Future<void> _saveGasTab() async {
+    await _preferencesService.setGasTab(state.selectedGasTab);
+  }
 
   void navigateTo(Screen newScreen) {
     if (newScreen != state.selectedScreen) {
       state = state.copyWith(selectedScreen: newScreen);
+      _saveScreen();
     }
   }
 
   void changeGasTabTo(ScreenTab newTab) {
     if (newTab != state.selectedGasTab) {
       state = state.copyWith(selectedGasTab: newTab);
+      _saveGasTab();
     }
   }
 
--- lib/src/navigation/data/providers.dart
+++ lib/src/navigation/data/providers.dart
@@ -1,10 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../services/preferences/preferences.dart';
+import '../../services/providers.dart';
 import 'navigation_controller.dart';
 import 'navigation_state.dart';
 
 final navigationControllerProvider =
     StateNotifierProvider.autoDispose<NavigationController, NavigationState>(
         (ref) {
-  return NavigationController();
+  final IPreferencesService service = ref.read(preferencesServiceProvider);
+  final controller = NavigationController(preferencesService: service);
+  controller.loadSavedState();
+  return controller;
 });
